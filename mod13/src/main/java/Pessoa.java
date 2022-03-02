43b6388fdac2c932b381a0f7c36f7bf550cf1445
Eduardo Longato <edulongato@yahoo.com.br>
2022-03-02 20:36:29 -0300

    Implementado as classes Pessoa, PessoaFisica e PessoaJuridica.

---
+++ mod13/src/main/java/Pessoa.java
@@ -0,0 +1,42 @@
+public abstract class Pessoa {
+
+    private String nome;
+    private String sobrenome;
+
+    private String nomeFantasia;
+
+    public void imprimirNomeSobrenome(){
+        System.out.println("Seu nome é: " + this.nome + this.sobrenome);
+    }
+
+    public void imprimirNomeFantasia(){
+        System.out.println("Nome fantasia é: " + this.nomeFantasia);
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+
+}
